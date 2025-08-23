11d757e63abdf4dcf3c9ef6ce339d92d16ddee5b
Lunitur <karlo.puselj@gmail.com>
2025-08-23 21:16:52 +0200

    add keybinds

--- users/carjin/foot.nix
+++ users/carjin/foot.nix
@@ -3,6 +3,12 @@
     enable = true;
     server.enable = true;
     settings = {
+      key-bindings = {
+        scrollback-up-half-page = "Control+Shift+k";
+        scrollback-down-half-page = "Control+Shift+j";
+        scrollback-up-line = "Control+k";
+        scrollback-down-line = "Control+j";
+      };
       colors = {
         # alpha = lib.mkForce 1;
         # background = "#E8E8E8";
