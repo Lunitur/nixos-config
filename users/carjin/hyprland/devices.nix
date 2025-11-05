a51bdfcdf62cbaf55f9c9e14db6d2c781e2286a5
Lunitur <karlo.puselj@gmail.com>
2025-11-05 21:26:16 +0100

    remove hyprland dotfiles

--- users/carjin/hyprland/devices.nix
+++
@@ -1,9 +0,0 @@
-{
-  wayland.windowManager.hyprland.settings = {
-    device = {
-      name = "logitech-g300s-optical-gaming-mouse";
-      accel_profile = "flat";
-      sensitivity = 0.4; # -1.0 - 1.0, 0 means no modification.
-    };
-  };
-}
