aa81488b61676791ad08e3ae509112b4b9e56a0d
Lunitur <karlo.puselj@gmail.com>
2025-11-12 15:28:17 +0100

    foot scroll keybind

--- users/carjin/foot.nix
+++ users/carjin/foot.nix
@@ -4,10 +4,10 @@
     server.enable = true;
     settings = {
       key-bindings = {
-        scrollback-up-half-page = "Control+Shift+k";
-        scrollback-down-half-page = "Control+Shift+j";
-        scrollback-up-line = "Control+k";
-        scrollback-down-line = "Control+j";
+        scrollback-up-half-page = "Control+k";
+        scrollback-down-half-page = "Control+j";
+        scrollback-up-line = "Control+Shift+k";
+        scrollback-down-line = "Control+Shift+j";
       };
       colors = {
         # alpha = lib.mkForce 1;
