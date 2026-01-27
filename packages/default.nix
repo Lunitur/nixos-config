da4270749f7c64bb61012d85924bf4cc3cdac003
Lunitur <karlo.puselj@gmail.com>
2026-01-27 20:11:16 +0100

    Fix: Restore legacy packages/default.nix for non-migrated hosts

--- packages/default.nix
+++ packages/default.nix
@@ -1,12 +1,10 @@
 { pkgs, ... }:
 {
   repl = pkgs.callPackage ./repl { };
-
   ammonite = pkgs.callPackage ./ammonite { };
-
   wl-ocr = pkgs.callPackage ./wl-ocr { };
-
-  mfcl3770cdw = pkgs.callPackage ./mfcl3770cdw { };
-
   hashcards = pkgs.callPackage ./hashcards { };
+  chatbox = pkgs.callPackage ./chatbox { };
+  nwg-launchers-assets = pkgs.callPackage ./nwg-launchers-assets { };
+  mfcl3730cdn = pkgs.callPackage ./mfcl3730cdn { };
 }
