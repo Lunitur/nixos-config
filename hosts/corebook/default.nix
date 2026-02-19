8952f9700f70d9d55f476b249de1b958dfb11b96
Lunitur <karlo.puselj@gmail.com>
2026-02-19 14:47:48 +0100

    performance profile corebook

--- hosts/corebook/default.nix
+++ hosts/corebook/default.nix
@@ -71,6 +71,7 @@ in
         # sudo tlp-stat to see current and possbile values
 
         # CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
         START_CHARGE_THRESH_BAT0 = 80;
         STOP_CHARGE_THRESH_BAT0 = 95;
@@ -80,9 +81,9 @@ in
         # INTEL_GPU_MIN_FREQ_ON_AC = 500;
         # INTEL_GPU_MIN_FREQ_ON_BAT = 500;
 
-        PLATFORM_PROFILE_ON_AC = "balanced";
+        PLATFORM_PROFILE_ON_AC = "performance";
         PLATFORM_PROFILE_ON_BAT = "low-power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
         CPU_HWP_DYN_BOOST_ON_AC = 1;
         CPU_HWP_DYN_BOOST_ON_BAT = 0;
