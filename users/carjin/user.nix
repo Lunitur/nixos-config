38ab63db5138b53dc9b9f9e7400ac72f4764cb2f
Lunitur <karlo.puselj@gmail.com>
2026-03-14 14:28:11 +0100

    enable tinjano public key

--- users/carjin/user.nix
+++ users/carjin/user.nix
@@ -22,7 +22,7 @@
     ]; # 'input' for kmonad
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhjgro/JnCCqYuHT/eiTv0EYqW8kN/MYL/nBmOJ+5l9 karlo.puselj@gmail.com"
-      # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAa4p4eaMH3uBaxjn+yQuD7TzvDzE/5gP3/Xshn1S0vj tinjano@proton.me"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAa4p4eaMH3uBaxjn+yQuD7TzvDzE/5gP3/Xshn1S0vj tinjano@proton.me"
     ];
   };
 
