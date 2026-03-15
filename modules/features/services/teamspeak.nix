064071262cdd6d3527af05295df13cfcc577ccf2
Lunitur <karlo.puselj@gmail.com>
2026-03-15 18:24:12 +0100

    teamspeak dendritic

--- modules/features/services/teamspeak.nix
+++ modules/features/services/teamspeak.nix
@@ -1,26 +1,37 @@
-{ config, pkgs, ... }:
 {
-  virtualisation.oci-containers.containers.teamspeak-server = {
-    # The actual systemd service will be named `container-teamspeak-server.service`.
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.features.services.teamspeak;
+in
+{
+  options.features.services.teamspeak.enable = lib.mkEnableOption "TeamSpeak server";
 
-    image = "teamspeaksystems/teamspeak6-server:latest";
+  config = lib.mkIf cfg.enable {
+    virtualisation.oci-containers.containers.teamspeak-server = {
+      # The actual systemd service will be named `container-teamspeak-server.service`.
 
-    autoStart = true;
+      image = "teamspeaksystems/teamspeak6-server:latest";
 
-    ports = [
-      "9987:9987/udp" # Voice Port
-      "30033:30033/tcp" # File Transfer
-    ];
+      autoStart = true;
 
-    environment = {
-      TSSERVER_LICENSE_ACCEPTED = "accept";
+      ports = [
+        "9987:9987/udp" # Voice Port
+        "30033:30033/tcp" # File Transfer
+      ];
+
+      environment = {
+        TSSERVER_LICENSE_ACCEPTED = "accept";
+      };
+
+      # Equivalent to `volumes`. This maps a directory on your NixOS host
+      # to a directory inside the container for persistent data.
+      volumes = [
+        "teamspeak-data:/var/tsserver"
+      ];
     };
-
-    # Equivalent to `volumes`. This maps a directory on your NixOS host
-    # to a directory inside the container for persistent data.
-    volumes = [
-      "teamspeak-data:/var/tsserver"
-    ];
   };
-
 }
