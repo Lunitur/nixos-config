94f9b4bec7e7993a949be6ad980e88768829cd66
Lunitur <karlo.puselj@gmail.com>
2026-03-16 21:20:16 +0100

    revert kernel change

--- hosts/victus/default.nix
+++ hosts/victus/default.nix
@@ -263,7 +263,7 @@ in
     };
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_stable;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod;
   boot.kernelParams = [
     "amd_iommu=on"
     "acpi_osi=Linux"
