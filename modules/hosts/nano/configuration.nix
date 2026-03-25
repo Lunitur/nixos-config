8ddaeb0c7147dc2c2d825a883b35394a4caee41f
Lunitur <karlo.puselj@gmail.com>
2026-03-25 21:17:44 +0100

    fix nano build

--- modules/hosts/nano/configuration.nix
+++ modules/hosts/nano/configuration.nix
@@ -5,32 +5,39 @@
 {
   flake.nixosConfigurations = inputs.self.lib.mkNixos "aarch64-linux" "nano";
 
-  flake.nixosModules.nano = {
-    imports = [
-      inputs.self.nixosModules.all
-      inputs.simple-nixos-mailserver.nixosModule
-      inputs.home-manager-stable.nixosModules.default
-      inputs.stylix.nixosModules.stylix
-      inputs.self.nixosModules.server
-      {
-        home-manager = {
-          users = {
-            carjin = {
-              imports = [
-                inputs.self.homeModules.server
-                inputs.self.homeModules.all
-              ];
-              home.username = "carjin";
-              home.homeDirectory = "/home/carjin";
-              home.stateVersion = "23.11";
-              programs.home-manager.enable = true;
+  flake.nixosModules.nano =
+    { pkgs-unstable, ... }:
+    {
+      imports = [
+        inputs.self.nixosModules.all
+        inputs.simple-nixos-mailserver.nixosModule
+        inputs.home-manager-stable.nixosModules.default
+        inputs.stylix.nixosModules.stylix
+        inputs.self.nixosModules.server
+        {
+          home-manager = {
+            extraSpecialArgs = {
+              inherit inputs;
+              inherit (inputs) self;
+              inherit pkgs-unstable;
             };
-          };
+            users = {
+              carjin = {
+                imports = [
+                  inputs.self.homeModules.server
+                  inputs.self.homeModules.all
+                ];
+                home.username = "carjin";
+                home.homeDirectory = "/home/carjin";
+                home.stateVersion = "23.11";
+                programs.home-manager.enable = true;
+              };
+            };
 
-          backupFileExtension = "backup";
-          useGlobalPkgs = true;
-        };
-      }
-    ];
-  };
+            backupFileExtension = "backup";
+            useGlobalPkgs = true;
+          };
+        }
+      ];
+    };
 }
