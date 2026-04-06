6971f64cd5b7cee6b46d4e9363b9c48e5d39cb70
Lunitur <karlo.puselj@gmail.com>
2026-04-07 00:11:09 +0200

    fix ani-cli

--- modules/features/common/packages.nix
+++ modules/features/common/packages.nix
@@ -13,7 +13,14 @@
       ++ (with pkgs-unstable; [
         tlrc
         nmap
-        ani-cli
+        (ani-cli.overrideAttrs (_: {
+          src = pkgs.fetchFromGitHub {
+            owner = "pystardust";
+            repo = "ani-cli";
+            tag = "v4.11";
+            hash = "sha256-gQprGtKXXpDm66dFWsrriL4G0NPav+nqm8T6wkdbgk8=";
+          };
+        }))
         protonup-ng
         heroic
       ])
