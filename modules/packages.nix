d7860af2749616795fcf13c7ab2fb6cba9028297
Lunitur <karlo.puselj@gmail.com>
2026-04-11 15:00:39 +0200

    nixpkgs nix run

--- modules/packages.nix
+++ modules/packages.nix
@@ -7,18 +7,24 @@
       brother = pkgs.callPackage ./_packages/mfcl3730cdn { };
     in
     {
-      packages = lib.filterAttrs (_: lib.isDerivation) (
-        {
-          repl = pkgs.callPackage ./_packages/repl { };
-          wl-ocr = pkgs.callPackage ./_packages/wl-ocr { };
-          hashcards = pkgs.callPackage ./_packages/hashcards { };
-          ammonite = ammonites.ammonite_3_5;
-        }
-        // ammonites
-        // lib.optionalAttrs pkgs.stdenv.hostPlatform.isx86 {
-          mfcl3730cdn-driver = brother.driver;
-          mfcl3730cdn-cupswrapper = brother.cupswrapper;
-        }
-      );
+      packages =
+        (lib.filterAttrs (_: lib.isDerivation) (
+          {
+            repl = pkgs.callPackage ./_packages/repl { };
+            wl-ocr = pkgs.callPackage ./_packages/wl-ocr { };
+            hashcards = pkgs.callPackage ./_packages/hashcards { };
+            ammonite = ammonites.ammonite_3_5;
+          }
+          // ammonites
+          // lib.optionalAttrs pkgs.stdenv.hostPlatform.isx86 {
+            mfcl3730cdn-driver = brother.driver;
+            mfcl3730cdn-cupswrapper = brother.cupswrapper;
+          }
+        ))
+        // {
+          nixpkgs = pkgs;
+        };
+
+      legacyPackages = pkgs;
     };
 }
