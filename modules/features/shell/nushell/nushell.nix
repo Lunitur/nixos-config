33aba943e7479db01df89e6c12b4d55b97878113
Lunitur <karlo.puselj@gmail.com>
2026-04-22 02:25:11 +0200

    keybinds update

--- modules/features/shell/nushell/nushell.nix
+++ modules/features/shell/nushell/nushell.nix
@@ -93,8 +93,8 @@
             nix-eval = "nix eval ~/nixos/#nixosConfigurations.${osConfig.networking.hostName}.config.system.build.toplevel.drvPath";
             nh-victus = "nh os switch ~/nixos -- --substituters \"http://cache.nixos.org http://victus\" ";
             em = "emacsclient -t";
-            gpro = "gemini -m gemini-3.1-pro-preview --include-directories ~/nixos";
-            gflash = "gemini -m gemini-3-flash-preview --include-directories ~/nixos";
+            gpro = "gemini-pro -m gemini-3.1-pro-preview --include-directories ~/nixos";
+            gflash = "gemini-pro -m gemini-3-flash-preview --include-directories ~/nixos";
           };
         };
         carapace.enable = true;
